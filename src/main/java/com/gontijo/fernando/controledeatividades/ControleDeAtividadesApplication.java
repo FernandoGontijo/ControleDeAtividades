095fe0fd455ef4992f886975d0e0a81fe8a4389e
FernandoGontijo <fernando.gontijos@hotmail.com>
2021-10-06 23:27:11 -0300

    Primeiro Commit

---
+++ src/main/java/com/gontijo/fernando/controledeatividades/ControleDeAtividadesApplication.java
@@ -0,0 +1,13 @@
+package com.gontijo.fernando.controledeatividades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeAtividadesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleDeAtividadesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gontijo/fernando/controledeatividades/ControleDeAtividadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gontijo.fernando.controledeatividades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeAtividadesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
